78b7541c2ca021dd22c8346bf75b2d3fed29dd4d
marios <mgeorgak859@gmail.com>
2022-10-26 00:29:52 +0300

    updates reservation component

--- admin/src/components/reservations/Reservations.jsx
+++ admin/src/components/reservations/Reservations.jsx
@@ -14,11 +14,45 @@ import { useLocation } from "react-router-dom";
 
 const Reservations = () => {
 
+    const location = useLocation();
+    const [restaurant, setRestaurant] = useState(location.state.restaurantId);
+    const { data, loading, error, reFetch } = useFetch(`/restaurants/reservations/${restaurant}`)
 
-
+    console.log(data)
     return (
-        <div>s</div>
+        <div>
+            <table>
+                <tr>
+                    <th>Table Number</th>
+                    <th>Reservations</th>
+                </tr>
+                {data.map((table, index) => <>
+
+                    <tr key={index} >
+                        <td> {table.number} </td>
+                        <td> {table.unavailableDates.map(date => {
+                            let unDateToRightDate = new Date(date.split('.')[0])
+                            let newDate = unDateToRightDate.toString().split(' ')
+                            newDate.pop()
+                            newDate.pop()
+                            newDate.pop()
+                            newDate.pop()
+                            newDate.pop()
+                            let searchedDateString = newDate.join(' ')
+                            console.log(searchedDateString)
+                            return <div className="reserve-dates">
+                                {searchedDateString}
+                                
+                            </div>
+                        })}  </td>
+                    </tr>
+
+                </>
+                )}
+
+            </table>
+        </div>
     )
 }
 
-    export default Reservations
+export default Reservations
